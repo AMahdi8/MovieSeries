ed80e3a0d9683afc7721e878b613f4d1e478aead
mahdi Aleboyeh <m.a8@mahdis-MacBook-Air.local>
2025-02-01 12:30:30 +0330

    changes on setting for deployment

--- MovieSeries/wsgi.py
+++ MovieSeries/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieSeries.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieSeries.settings.prod')
 
 application = get_wsgi_application()
